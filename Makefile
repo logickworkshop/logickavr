a273fdf2ada51df8e4494715c72eda05dc65b06c
Aaron Mavrinac <mavrinac@gmail.com>
2015-02-03 00:39:33 -0500

    Makefile for compiling as library archive.

---
+++ Makefile
@@ -0,0 +1,33 @@
+PROJECT= logickavr
+
+MMCU= atmega168
+F_CPU= 1000000UL
+
+AR= avr-ar
+CC= avr-gcc
+CFLAGS= -Os -Wall -mmcu=$(MMCU) -DF_CPU=$(F_CPU) -I/home/aaron/projects/logickavr/include
+
+BUILDDIR= build
+LIBDIR= lib
+
+COMPONENTS= spi
+
+.PHONY: clean
+
+all: $(BUILDDIR) $(LIBDIR) $(LIBDIR)/lib$(PROJECT).a
+
+$(LIBDIR)/liblogickavr.a: $(foreach component,$(COMPONENTS),$(BUILDDIR)/$(component).o)
+	$(AR) -rcs $@ $^
+	avr-size --common $@
+
+$(BUILDDIR)/%.o: src/%.c
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+build:
+	mkdir -p $(BUILDDIR)
+
+lib:
+	mkdir -p $(LIBDIR)
+
+clean:
+	rm -rf $(BUILDDIR) $(LIBDIR)
--- src/spi.c
+++ src/spi.c
@@ -20,42 +20,10 @@
  * Aaron Mavrinac <aaron@logick.ca>
  */
 
-#ifndef LW_SPI_H_
-#define LW_SPI_H_
+#include <logickavr/spi.h>
 
-#include <avr/io.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// SPI pin definitions
-#if (defined(__AVR_AT90USB82__) || \
-     defined(__AVR_AT90USB162__))
-  #define LW_SPI_PIN_SCK        PORTB1
-  #define LW_SPI_PIN_MOSI       PORTB2
-  #define LW_SPI_PIN_MISO       PORTB3
-  #define LW_SPI_PIN_SS         PORTB0
-#elif (defined(__AVR_ATmega48__) || \
-       defined(__AVR_ATmega88__) ||  \
-       defined(__AVR_ATmega168__) || \
-       defined(__AVR_ATmega328__) || \
-       defined(__AVR_ATmega328P__))
-  #define LW_SPI_PIN_SCK        PORTB5
-  #define LW_SPI_PIN_MOSI       PORTB3
-  #define LW_SPI_PIN_MISO       PORTB4
-  #define LW_SPI_PIN_SS         PORTB2
-#endif
-
-/**
- * Send a byte via SPI.
- *
- * data: Byte to send.
- */
-void lw_spi_sendbyte(char data);
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
-
-#endif // LW_SPI_H_
+void lw_spi_sendbyte(char data)
+{
+  SPDR = data;
+  while(!(SPSR & (1 << SPIF)));
+}
