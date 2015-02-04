edb897b96047bd0838d0797ca6f6346fba7a4182
Aaron Mavrinac <mavrinac@gmail.com>
2015-02-03 20:35:27 -0500

    Flesh out MIDI module.

--- include/logickavr/midi.h
+++ include/logickavr/midi.h
@@ -23,6 +23,8 @@
 #ifndef LW_MIDI_H_
 #define LW_MIDI_H_
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -33,9 +35,9 @@ extern "C" {
 #define LW_MIDI_CMD_NOTE_OFF      0x80
 #define LW_MIDI_CMD_NOTE_ON       0x90
 #define LW_MIDI_CMD_AFTERTOUCH    0xA0
-#define LW_MIDI_CMD_CONTINUOUS    0xB0
-#define LW_MIDI_CMD_PATCH         0xC0
-#define LW_MIDI_CMD_CH_PRESSURE   0xD0
+#define LW_MIDI_CMD_CC            0xB0
+#define LW_MIDI_CMD_PC            0xC0
+#define LW_MIDI_CMD_PRESSURE      0xD0
 #define LW_MIDI_CMD_PITCH_BEND    0xE0
 
 // MIDI system messages
@@ -51,6 +53,21 @@ extern "C" {
 #define LW_MIDI_SYS_STOP          0xFC
 #define LW_MIDI_SYS_ACT_SENSING   0xFE
 #define LW_MIDI_SYS_RESET         0xFF
+
+/**
+ * Send a MIDI message.
+ *
+ * cmd: The command to send (channel command bitwise-OR'd with channel, or system message).
+ */
+void lw_midi_sendmsg(char cmd, ...);
+
+/**
+ * Send a MIDI system exclusive message.
+ *
+ * data: Pointer to the start of the data buffer to send.
+ * n: Number of bytes to send.
+ */
+void lw_midi_sendsysex(const char * data, size_t n);
 
 #ifdef __cplusplus
 } // extern "C"
---
+++ src/midi.c
@@ -0,0 +1,71 @@
+/*
+ * ####                                                ####
+ * ####                                                ####      ##
+ * ####                                                ####    ####
+ * ####                                                ####  ####
+ * ####  ############  ############  ####  ##########  ########
+ * ####  ####    ####  ####    ####  ####  ####        ########
+ * ####  ####    ####  ####    ####  ####  ####        ####  ####
+ * ####  ####    ####  ####    ####  ####  ####        ####    ####
+ * ####  ####    ####  ####    ####  ####  ####        ####      ####
+ * ####  ############  ############  ####  ##########  ####        ####
+ *                             ####                                  ####
+ * ################################                                    ####
+ *            __      __              __              __      __         ####
+ *   |  |    |  |    [__)    |_/     (__     |__|    |  |    [__)          ####
+ *   |/\|    |__|    |  \    |  \    .__)    |  |    |__|    |               ####
+ *                                                                             ##
+ *
+ * AVR library routines for MIDI (musical instrument digital interface).
+ * Aaron Mavrinac <aaron@logick.ca>
+ */
+
+#include <stdarg.h>
+
+#include <logickavr/midi.h>
+#include <logickavr/usart.h>
+
+void lw_midi_sendmsg(char cmd, ...)
+{
+  // TODO: send byte cmd
+
+  va_list ap;
+  va_start(ap, cmd);
+
+  switch((cmd & 0xF0) == 0xF0 ? cmd : cmd & 0xF0)
+  {
+    // two-argument commands
+    case LW_MIDI_CMD_NOTE_OFF:
+    case LW_MIDI_CMD_NOTE_ON:
+    case LW_MIDI_CMD_AFTERTOUCH:
+    case LW_MIDI_CMD_CC:
+    case LW_MIDI_CMD_PITCH_BEND:
+    case LW_MIDI_SYS_SONG_PP:
+      // TODO: send byte va_arg(ap, char)
+    // one-argument commands
+    case LW_MIDI_CMD_PC:
+    case LW_MIDI_CMD_PRESSURE:
+    case LW_MIDI_SYS_TC_QF:
+    case LW_MIDI_SYS_SONG_SEL:
+      // TODO: send byte va_arg(ap, char)
+      break;
+  }
+
+  va_end(ap);
+}
+
+void lw_midi_sendsysex(const char * data, size_t n)
+{
+  size_t i;
+
+  // send system exclusive start message
+  lw_midi_sendmsg(LW_MIDI_SYS_SE_START);
+
+  for(i = 0; i < n; ++i)
+  {
+    // TODO: send byte data[i]
+  }
+
+  // send system exclusive end message
+  lw_midi_sendmsg(LW_MIDI_SYS_SE_END);
+}
